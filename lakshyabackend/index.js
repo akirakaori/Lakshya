8cc74fe530aa9b86d2ab11dee09b18dc3f3be0ef
akirakaori <ashikakambang@gmail.com>
2025-12-26 16:10:50 +0545

    Connection of backend to the MondoDB (successful)

--- lakshyabackend/index.js
+++ lakshyabackend/index.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+require('./models/database');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.get('/lakshya', (req, res) => {
+    res.send('Welcome to Lakshya Backend!');
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
