d10f23b125fbab477e8667f4edb1b76dc096692e
akirakaori <ashikakambang@gmail.com>
2025-12-28 02:38:37 +0545

    the new dashboard based on the roles were created and were navigated as per the role.

---
+++ lakshyafrontend/src/pages/RecruiterDashboard.tsx
@@ -0,0 +1,10 @@
+function RecruiterDashboard() {
+  return (
+    <div>
+      <h2>Recruiter Dashboard</h2>
+      <p>Post jobs, manage candidates</p>
+    </div>
+  );
+}
+
+export default RecruiterDashboard;
