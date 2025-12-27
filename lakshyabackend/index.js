f16eac9560d535bf5ae58856a17c358260573951
akirakaori <ashikakambang@gmail.com>
2025-12-27 19:35:50 +0545

    Successfel connection of the API ( Sign up successully saved into the database)

--- lakshyabackend/index.js
+++ lakshyabackend/index.js
@@ -23,6 +23,27 @@ app.use('/auth',AuthRouter);
 app.get('/lakshya', (req, res) => {
     res.send('Welcome to Lakshya Backend!');
 });
+
+// SIGNUP API (THIS IS IMPORTANT)
+// app.post('/auth/signup', (req, res) => {
+//   const { name, email, number, password } = req.body;
+
+//   if (!name || !email || !number || !password) {
+//     return res.status(400).json({
+//       success: false,
+//       message: 'All fields are required',
+//     });
+//   }
+
+//   return res.status(201).json({
+//     success: true,
+//     message: 'Signup successful',
+//     data: { name, email },
+//   });
+// });
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
