e103ee169fc4bf316909e10a3c8da83880b41c58
akirakaori <ashikakambang@gmail.com>
2026-02-16 19:52:26 +0545

    saved index.ts part

--- lakshyafrontend/src/pages/recruiter/index.ts
+++ lakshyafrontend/src/pages/recruiter/index.ts
@@ -4,4 +4,3 @@ export { default as ManageJobs } from './manage-jobs';
 export { default as JobApplications } from './job-applications';
 export { default as CandidateProfile } from './candidate-profile';
 export { default as RecruiterProfile } from './profile';
-
