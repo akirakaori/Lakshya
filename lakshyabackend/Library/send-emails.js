4bd49d92ca397570f1a9bb5b2bf94feea9a58a6f
Ashika Kambang <ashikakambang@gmail.com>
2026-04-20 13:20:57 +0545

    chanegd the name

--- lakshyabackend/Library/send-emails.js
+++ lakshyabackend/Library/send-emails.js
@@ -19,7 +19,7 @@ const sendEmail = async (to, subject, text) => {
   });
 
   const mailOptions = {
-    from: process.env.BREVO_FROM,
+    from: `"Lakshya" <${process.env.BREVO_FROM}>`,
     to,
     subject,
     text,
